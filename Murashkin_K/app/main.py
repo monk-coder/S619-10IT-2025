eaceb240a26e1e2da2f276156667a900792869b4
L0RIk1 <145850532+L0RIk1@users.noreply.github.com>
2025-10-22 10:04:03 +0300

    Update main.py

--- Murashkin_K/app/main.py
+++ Murashkin_K/app/main.py
@@ -221,4 +221,5 @@ async def export_contacts_csv(
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=9999, reload=True)
+
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)

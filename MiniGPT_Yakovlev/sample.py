a18ab8d7093e1f7a49b2037ee34b07693f7e85ac
Alexander459325 <letoanton3@gmail.com>
2026-03-04 09:18:50 +0300

    Update sample.py

--- MiniGPT_Yakovlev/sample.py
+++ MiniGPT_Yakovlev/sample.py
@@ -9,11 +9,5 @@ if __name__ == "__main__":
     parser.add_argument('--top_k', type=int, default=None)
     parser.add_argument('--checkpoint', type=str, default=None)
     args = parser.parse_args()
-    result = sample(
-        args.prompt,
-        args.max_new_tokens,
-        args.temperature,
-        args.top_k,
-        args.checkpoint
-    )
+    result = sample(args.prompt, args.max_new_tokens, args.temperature, args.top_k, args.checkpoint)
     print(f"\nPrompt: \"{args.prompt}\"\n{'-'*60}\n{result}\n{'-'*60}")

c006efa6ea00b76d5ca9f39aa67aae9214a76908
Alexander459325 <letoanton3@gmail.com>
2026-03-11 09:57:26 +0300

    Update config.py

--- MiniGPT_Yakovlev/config.py
+++ MiniGPT_Yakovlev/config.py
@@ -5,8 +5,6 @@ TOKENIZER_PATH = "tokenizer.json"
 NUM_MERGES = 500
 VAL_SPLIT = 0.1
 
-GITHUB_DATA_URL = "https://github.com/monk-coder/S619-10IT-2025/blob/main/0/data.txt"
-
 VOCAB_SIZE = None
 D_MODEL = 64
 N_HEAD = 2
